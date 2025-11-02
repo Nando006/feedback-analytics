803f84f4bbd9662d89da9a9f2a8d2ca2229d3042
Nando <FSNando.012@outlook.com>
2025-11-02 11:09:29 -0300

    refatorando o componente EditProfile, corrigindo a importação do componente Information

--- pages/user/edit/editProfile.tsx
+++ pages/user/edit/editProfile.tsx
@@ -1,12 +1,12 @@
 import Header from 'components/user/profile/editUser/header';
-import Info from 'components/user/profile/editUser/information';
+import Information from 'components/user/profile/editUser/information';
 import Preferences from 'components/user/profile/editUser/preferences';
 
 export default function EditProfile() {
   return (
     <div className="font-inter space-y-6">
       <Header />
-      <Info />
+      <Information />
       <Preferences />
     </div>
   );
