98c538a66074f3a8700d2a4c4cf71a52281d9778
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-02-25 09:08:42 -0300

    Atualiza design do componente fieldsQRCode\fieldMessage.tsx
    - Inserção de variáveis de cor no componente fieldMessage.tsx.
    - Inserção de fontes nos textos do componente.

--- components/public/forms/fields/fieldsQRCode/fieldMessage.tsx
+++ components/public/forms/fields/fieldsQRCode/fieldMessage.tsx
@@ -6,7 +6,7 @@ export default function FieldMessage({ message, onMessageChange }: FieldMessageP
     <div>
       <label
         htmlFor="message"
-        className="block text-sm font-medium text-neutral-200 mb-2">
+        className="font-work-sans block text-sm font-medium text-(--text-primary) mb-2">
         Conte-nos mais sobre sua experiência
       </label>
       <textarea
@@ -15,7 +15,7 @@ export default function FieldMessage({ message, onMessageChange }: FieldMessageP
         onChange={(e) => onMessageChange(e.target.value)}
         placeholder="Descreva sua experiência, sugestões ou comentários..."
         rows={4}
-        className="w-full px-4 py-3 border border-neutral-700 rounded-lg bg-neutral-800 text-neutral-100 placeholder-neutral-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent resize-none"
+        className="font-work-sans w-full px-4 py-3 border border-(--container-border) rounded-lg bg-(--container-secondary) text-(--text-primary) placeholder-(--text-secondary) focus:outline-none focus:ring-2 focus:ring-(--primary-color) focus:border-transparent resize-none"
         required
       />
     </div>
